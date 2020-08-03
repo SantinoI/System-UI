5ec2eafe674d53afbe535f5e1850d3c565122c87
Santino Isgrò <santiisgro96@gmail.com>
2020-08-03 12:31:15 +0200

    FIX

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -66,7 +66,7 @@ def input():
             equations['G3']['stabilita'] = stab3
             return jsonify({"tempi":vettore_tempi,"output":vettore_output,"ref":ref_array,"equations":equations})
         else:
-            vettore_tempi,vettore_output, auto_val_1, auto_val_2, auto_val_3, stab1, stab2, stab3 = schema3_evaluate(equations,time,input,controller)
+            vettore_tempi,vettore_output, auto_val_1, auto_val_2, auto_val_3, stab1, stab2, stab3 = schema3_evaluate(equations,time,input)
             equations['G1']['autovalori'] = str(auto_val_1)
             equations['G1']['stabilita'] = stab1
             equations['G2']['autovalori'] = str(auto_val_2)
