dd91aa3481acc843f012fdd4a501a7403ad3d2b2
Santino Isgrò <santiisgro96@gmail.com>
2020-08-03 10:35:56 +0200

    fix2

--- flaskapp/sistemi.py
+++ flaskapp/sistemi.py
@@ -72,7 +72,7 @@ def stability(aut1,aut2,y1,y):
     aut1 = aut1.split()[0]
     print(aut1)
     print(aut2)
-    if(float(y1) < 0 and float(y) > 0):  
+    if(float(y1) < 0 and float(y) > 0 and " " in aut2):  
         aut2 = aut2.rsplit(" ",1)[1]
     else:
         aut2 = aut2.split()[0]
